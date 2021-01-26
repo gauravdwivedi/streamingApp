37e08533e5400ba80f279e63a44f29a02b13c82e
gauravdwivedi <gauravdwivedi@hotmail.com>
2021-01-27 00:30:38 +0530

    delete works

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,10 +4,7 @@ import history from '../history';
 
 const Modal = (props) => {
   return ReactDom.createPortal(
-    <div
-      onClick={() => history.push('/')}
-      className="ui dimmer modals visible active"
-    >
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       <div
         onClick={(e) => e.stopPropagation()}
         className="ui standard modal visible active"
