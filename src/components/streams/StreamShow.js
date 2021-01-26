f3bacb578cedaf54adff2544f3862dd6b26c0db2
gauravdwivedi <gauravdwivedi@hotmail.com>
2021-01-27 02:20:07 +0530

    added componentwillunmount to cleanup videoplayer resources and filter current user's streams only

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -16,6 +16,10 @@ class StreamShow extends React.Component {
 
     this.buildPlayer();
   }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
   componentDidUpdate() {
     this.buildPlayer();
   }
